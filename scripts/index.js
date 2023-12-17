d3436ce1eb0e90fd638280a4b83b617a3655d188
Vladlena Orlova <vladlena.bossa@gmail.com>
2023-12-17 10:25:30 +0300

    new коммит

--- scripts/index.js
+++ scripts/index.js
@@ -6,28 +6,26 @@ const content = document.querySelector('.content');
 const list = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(cardName, cardLink) {
+function createCard(cardName, cardLink, callbackCard) {
     const cardElement = template.querySelector('.card').cloneNode(true);
 
     const cardImage = cardElement.querySelector('.card__image');
     cardImage.src = cardLink;
+    cardImage.alt = cardName;
 
     const cardTitle = cardElement.querySelector('.card__title');
-    cardTitle.alt = cardName;
     cardTitle.textContent = cardName;
 
 const deleteButton = cardElement.querySelector('.card__delete-button');
-deleteButton.addEventListener('click', () => deleteCard(cardElement));
-function deleteCard(cardElement) {  
-    cardElement.remove(); 
-}; 
+deleteButton.addEventListener('click', (callbackCard) => deleteCard(cardElement));
 
 return cardElement;
-}
+};
 
 // @todo: Функция удаления карточки
-function deleteCard(evt){
-evt.target.closest('.card').remove();}
+function deleteCard(cardElement) {
+cardElement.remove(); 
+};
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function ({name, link}) {
