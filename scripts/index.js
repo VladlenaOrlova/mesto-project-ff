2b5b1cc6e2928cd4cc4c9a094114240692453c9e
Vladlena Orlova <vladlena.bossa@gmail.com>
2023-12-21 00:11:24 +0300

    исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
-// @todo: Темплейт карточки
-const template = document.querySelector('#card-template').content;
+// @todo: Темплейт карточки 
+const template = document.querySelector('#card-template').content; 
+// @todo: DOM узлы 
+const content = document.querySelector('.content'); 
+const list = document.querySelector('.places__list'); 
 
-// @todo: DOM узлы
-const content = document.querySelector('.content');
-const list = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-function createCard(cardName, cardLink, callbackCard) {
-    const cardElement = template.querySelector('.card').cloneNode(true);
+// @todo: Функция создания карточки 
+function createCard(cardName, cardLink , deleteCard) { 
+    const cardElement = template.querySelector('.card').cloneNode(true); 
 
     const cardImage = cardElement.querySelector('.card__image');
     cardImage.src = cardLink;
     cardImage.alt = cardName;
 
     const cardTitle = cardElement.querySelector('.card__title');
-    cardTitle.textContent = cardName;
+    cardTitle.textContent = cardName;  
 
-const deleteButton = cardElement.querySelector('.card__delete-button');
-deleteButton.addEventListener('click', (callbackCard) => deleteCard(cardElement));
+const deleteButton = cardElement.querySelector('.card__delete-button'); 
+deleteButton.addEventListener('click', deleteCard);
 
-return cardElement;
-};
+return cardElement; 
+} 
 
-// @todo: Функция удаления карточки
-function deleteCard(cardElement) {
-cardElement.remove(); 
-};
+// @todo: Функция удаления карточки 
+function deleteCard(evt){ 
+evt.target.closest('.card').remove(); 
+} 
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function ({name, link}) {
